3ce940d49c44c545d3127718ecbac93b3ab91301
Ilias Marinos <ilias@marinos.io>
2020-03-26 15:18:41 +0200

    Normalize incidents plot by million inhabitants.

--- plot.py
+++ plot.py
@@ -29,21 +29,27 @@ tmpData = allData[allData['Country/Region'].isin(countries)].loc[allData['Countr
 startDate = tmpData.loc[tmpData['CumCases'] != 0].iloc[0,:]['Date'] - timedelta(days=1)
 print(startDate)
 
+
 fig, (ax1, ax2) = plt.subplots(nrows=1,ncols=2, sharex=True)
 
 for country, col in zip(countries, colors):
+  # Get country's population.
+  population = popData.loc[popData['name'] == country,'pop2020'].to_numpy()[0] 
+
   c_df = allData.loc[(allData['Country/Region'] == country) &
                     (allData['Province/State'] == country) & (allData['Date'] >= startDate)].copy()
   c_df['CasesGrowthR'] = c_df['CumCases'].pct_change(periods=1)*100
   c_df['CasesGrowthR'].fillna(0.0, inplace=True)
+  c_df['CasesPerMillion'] = c_df['CumCases']/population
+  c_df['DeathsPerMillion'] = c_df['CumDeaths']/population
   c_df.reset_index(inplace=True)
 
   # Plot the data.
-  c_df.plot(ax=ax1, kind='line', x='Date', y='CumCases', color=col, label=country + "(Cases)", logy=True, legend=True, grid=True)
-  c_df.plot(ax=ax1, kind='line', linestyle='--', x='Date', y='CumDeaths', color=col, label=country + "(Deaths)", logy=True, legend=True, grid=True)
+  c_df.plot(ax=ax1, kind='line', x='Date', y='CasesPerMillion', color=col, label=country + "(Cases)", logy=True, legend=True, grid=True)
+  c_df.plot(ax=ax1, kind='line', linestyle='--', x='Date', y='DeathsPerMillion', color=col, label=country + "(Deaths)", logy=True, legend=True, grid=True)
   c_df.plot(ax=ax2, kind='line', x='Date', y='CasesGrowthR', yticks=range(0, 600, 20), color=col, label=country, legend=True, grid=True)
 
-ax1.set_ylabel("# of Confirmed Incidents")
+ax1.set_ylabel("# of Confirmed Incidents Per Million Inhabitants")
 ax2.set_ylabel("Cases Growth Rate (%)")
 
 # plt.tight_layout()
